bb65779e8d2689ca8d3434e81d534cb6aeb085bd
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2017-12-31 13:25:51 -0200

    Some SudokuGame functions implemented. Missing correct constructor.

--- ElementLib/source/SudokuGame.cpp
+++ ElementLib/source/SudokuGame.cpp
@@ -46,6 +46,38 @@ bool SudokuGame::isValid(void)
 	return vality;
 }
 
+bool SudokuGame::isSolved(void)
+{
+	bool vality;
+	vality = this->isValid();
+	for( unsigned row=0 ; row<this->size ; ++row )
+		for( unsigned column=0 ; column<this->size ; ++column )
+				if( this->getCellValue(row,column)==0) vality=false;
+	return vality;
+}
+
+void SudokuGame::solverForOnePossibleValue(void)
+{
+	bool aCellWasSet;
+	unsigned uniquePossibleValue;
+	do
+	{
+		aCellWasSet = false;
+		for( unsigned row=0 ; row<this->size ; ++row )
+		{
+			for( unsigned column=0 ; column<this->size ; ++column )
+			{
+				if( this->getCellNumberOfPossibleValues(row,column) == 1 )
+				{
+					uniquePossibleValue = this->getCellUniquePossibleValue(row,column);
+					this->setCellValue(row,column,uniquePossibleValue);
+					aCellWasSet = true;
+				}
+			}
+		}
+	}while( aCellWasSet==true );
+}
+
 void SudokuGame::allocCells(unsigned s)
 {
 	// s : game size
@@ -108,26 +140,23 @@ void SudokuGame::allocRectangles(void)
 	}
 }
 
-void SudokuGame::solverForOnePossibleValue(void)
+void SudokuGame::set(const std::vector< std::vector<unsigned> > data)
 {
-	bool aCellWasSet;
-	unsigned uniquePossibleValue;
-	do
+	{// size verification
+		if (data.size() != this->size)
+			throw "Invalid matrix row size - set sudoku game values.";
+		for(unsigned row=0 ; row<this->size ; ++row)
+			if (data[row].size() != this->size)
+				throw "Invalid matrix column size - set sudoku game values.";
+	}
+	for (unsigned row=0 ; row<this->size ; ++row)
 	{
-		aCellWasSet = false;
-		for( unsigned row=0 ; row<N_VALUES ; ++row )
+		for (unsigned column=0 ; column<this->size ; ++column)
 		{
-			for( unsigned column=0 ; column<N_VALUES ; ++column )
-			{
-				if( this->getCellNumberOfPossibleValues(row,column) == 1 )
-				{
-					uniquePossibleValue = this->getCellUniquePossibleValue(row,column);
-					this->setCellValue(row,column,uniquePossibleValue);
-					aCellWasSet = true;
-				}
-			}
+			setCellValue(row , column , data[row][column] );
 		}
-	}while( aCellWasSet==true );
+	}
+	return;
 }
 
 void SudokuGame::buildCheck(unsigned s)
@@ -200,43 +229,25 @@ void SudokuGame::set(const vector<unsigned> data)
 	return;
 }
 
-void SudokuGame::set(const vector< vector<unsigned> > data)
-{
-	if (data.size() != N_VALUES)
-		throw "Invalid matrix row size - set sudoku game values.";
-	for(unsigned r=0 ; r<N_VALUES ; ++r)
-		if (data[r].size() != N_VALUES)
-			throw "Invalid matrix column size - set sudoku game values.";
-
-	for (unsigned r=0 ; r<N_VALUES ; ++r)
-	{
-		for (unsigned c=0 ; c<N_VALUES ; ++c)
-		{
-			setCellValue(r,c,data[r][c]);
-		}
-	}
-	return;
-}
-
-void SudokuGame::write(const char * const fileName)
+void SudokuGame::createFile(const char * const fileName, const char * datasetName) const
 {
 	hid_t file;
 	hid_t dataset, dataspace;
 	const hid_t datatype = H5T_NATIVE_UINT;
 	const hsize_t rank = 2;
 	hsize_t dataspace_size[2];
-	unsigned data[N_VALUES][N_VALUES];
+	unsigned data[SudokuGame::size][SudokuGame::size];
 	unsigned row, column;
 
-	for (row=0 ; row<N_VALUES ; ++row)
-		for(column=0 ; column<N_VALUES ; ++column)
+	for (row=0 ; row<SudokuGame::size ; ++row)
+		for(column=0 ; column<SudokuGame::size ; ++column)
 			data[row][column] = this->getCellValue(row,column);
 
-    file = H5Fcreate(fileName, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT); if(file<0) throw "Error opening file to write.";
-	dataspace_size[0] = N_VALUES;
-	dataspace_size[1] = N_VALUES;
+    file = H5Fcreate(fileName, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT); if(file<0) throw "Error creating file to write.";
+	dataspace_size[0] = SudokuGame::size;
+	dataspace_size[1] = SudokuGame::size;
 	dataspace = H5Screate_simple(rank, dataspace_size, NULL); if(dataspace<0) throw "Error opening dataspace to save file.";
-	dataset = H5Dcreate2(file, "sudoku game", datatype, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT); if(dataset<0) throw "Error opening dataset to save file.";
+	dataset = H5Dcreate2(file, datasetName, datatype, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT); if(dataset<0) throw "Error opening dataset to save file.";
 	H5Dwrite(dataset, datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
 
 	H5Dclose(dataset);
@@ -245,20 +256,47 @@ void SudokuGame::write(const char * const fileName)
 	return;
 }
 
-void SudokuGame::read(const char * const fileName)
+void SudokuGame::appendFile(const char * const fileName, const char * datasetName) const
+{
+	hid_t file;
+	hid_t dataset, dataspace;
+	const hid_t datatype = H5T_NATIVE_UINT;
+	const hsize_t rank = 2;
+	hsize_t dataspace_size[2];
+	unsigned data[SudokuGame::size][SudokuGame::size];
+	unsigned row, column;
+
+	for (row=0 ; row<SudokuGame::size ; ++row)
+		for(column=0 ; column<SudokuGame::size ; ++column)
+			data[row][column] = this->getCellValue(row,column);
+
+    file = H5Fopen(fileName, H5F_ACC_RDWR, H5P_DEFAULT); if(file<0) throw "Error opening file to write.";
+	dataspace_size[0] = SudokuGame::size;
+	dataspace_size[1] = SudokuGame::size;
+	dataspace = H5Screate_simple(rank, dataspace_size, NULL); if(dataspace<0) throw "Error opening dataspace to save file.";
+	dataset = H5Dcreate2(file, datasetName, datatype, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT); if(dataset<0) throw "Error opening dataset to save file.";
+	H5Dwrite(dataset, datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+
+	H5Dclose(dataset);
+	H5Sclose(dataspace);
+	H5Fclose(file);
+	return;
+}
+
+void SudokuGame::readFromFile(const char * const fileName, const char * datasetName)
 {
 	hid_t file;
 	hid_t dataset;
 	const hid_t datatype = H5T_NATIVE_UINT;
-	unsigned data[N_VALUES][N_VALUES];
+	unsigned data[SudokuGame::size][SudokuGame::size];
 	unsigned row, column;
 
     file = H5Fopen(fileName, H5F_ACC_RDONLY, H5P_DEFAULT); if(file<0) throw "Error opening file to read.";
-	dataset = H5Dopen2(file, "sudoku game", H5P_DEFAULT); if(dataset<0) throw "Error opening dataset to read file.";
+	dataset = H5Dopen2(file, datasetName, H5P_DEFAULT); if(dataset<0) throw "Error opening dataset to read file.";
 	H5Dread(dataset, datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, data );
 
-	for (row=0 ; row<N_VALUES ; ++row)
-		for(column=0 ; column<N_VALUES ; ++column)
+	for (row=0 ; row<SudokuGame::size; ++row)
+		for(column=0 ; column<SudokuGame::size ; ++column)
 			this->setCellValue(row,column,data[row][column]);
 
     H5Dclose(dataset);
@@ -269,8 +307,8 @@ void SudokuGame::read(const char * const fileName)
 bool operator==(SudokuGame lhs , SudokuGame rhs)
 {
 	unsigned r, c;
-	for(r=0 ; r<N_VALUES ; ++r)
-		for(c=0 ; c<N_VALUES ; ++c)
+	for(r=0 ; r<SudokuGame::size ; ++r)
+		for(c=0 ; c<SudokuGame::size ; ++c)
 			if(lhs.getCellValue(r,c) != rhs.getCellValue(r,c)) return false;
 	return true;
 }
