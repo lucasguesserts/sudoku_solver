ae7f9b4b81d6ac7cf3e6065d6f4a91bfcf14ad4b
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-01-04 19:06:27 -0200

    Added fucntion in App to verify if the sudoku game is valid.
    The function was implemented but not tested.

--- Apps/source/sudokuApps.cpp
+++ Apps/source/sudokuApps.cpp
@@ -14,6 +14,7 @@
 
 const char * problemDatasetName = "Problem";
 void createEmptySudokuGame(const char * fileName);
+void checkSudokuGame(const char * filename);
 
 int main(const int argc, const char * const argv[])
 {
@@ -38,4 +39,16 @@ void createEmptySudokuGame(const char * fileName)
 	std::cout << std::endl << "Successfully created empty sudoku game '" << fileNameWithExtension << "'." << std::endl;
 	std::cout << std::endl << "Use hdfview to edit the file." << std::endl;
 	std::cout << std::endl;
+	return;
+}
+
+void checkSudokuGame(const char * filename, const char * datasetName)
+{
+	SudokuGame sg;
+	sg.readFromFile(fileName, datasetName);
+	if (sg.isValid())
+		std::cout << "File" << " '" << fileName << "/" << datasetName << "' " << "is VALID." << std::endl << std::endl;
+	else
+		std::cout << "File" << " '" << fileName << "/" << datasetName << "' " << "is INVALID." << std::endl << std::endl;
+	return;
 }
