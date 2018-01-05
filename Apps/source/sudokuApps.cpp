ffb613166382a3cf5813554dbcfd9f5570f2dbe9
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-01-05 19:05:04 -0200

    It was tried to write the program, but errors occurred. They must be solved.

--- Apps/source/sudokuApps.cpp
+++ Apps/source/sudokuApps.cpp
@@ -16,12 +16,48 @@
 
 int main(const int argc, const char * const argv[])
 {
-	if(argc!=2)
+	const char * fileName = argv[2];
+	if(argc != 3)
 	{
 		std::cerr << std::endl << "Invalid number of arguments. Use:" << std::endl;
-		std::cerr << std::endl << "\t$ program file_name" << std::endl << std::endl;
+		std::cerr << std::endl << "\t$ program behaviour file_name" << std::endl;
+		std::cerr << std::endl << "\tbehaviour = create_empty, check, solved, solve." << std::endl << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	createEmptySudokuGame(argv[1]);
+	switch(argv[1])
+		case "create_empty":
+			createEmptySudokuGame(argv[2]);
+			break;
+		case "check":
+			const char * datasetName = AppDefinition::problemDatasetName;
+			std::cout << "'" << filename << "/" << datasetName << "' ";
+			if( checkIsValidSudokuGame(fileName,datasetName) )
+				std::cout << "is VALID." << std::endl << std::endl;
+			else
+				std::cout << "is INVALID." << std::endl << std::endl;
+			break;
+		case "solved":
+			const char * datasetName = AppDefinition::solutionDatasetName;
+			std::cout << "'" << filename << "/" << datasetName << "' ";
+			if( checkIsSolvedSudokuGame(fileName,datasetName) )
+				std::cout << "is SOLVED." << std::endl << std::endl;
+			else
+				std::cout << "is UNSOLVED ." << std::endl << std::endl;
+			break;
+		case "solve":
+			bool wasSolved;
+			const char * datasetName = AppDefinition::problemDatasetName;
+			wasSolved = solveSudokuGame(fileName,datasetName) 
+			std::cout << "'" << filename << "/" << datasetName << "' ";
+			if(wasSolved)
+				std::cout << "SOLVED." << std::endl << std::endl;
+			else
+				std::cout << "CANNOT be SOLVED." << std::endl << std::endl;
+			break;
+		default:
+			std::cerr << std::endl << "Invalid behaviour. Use:" << std::endl;
+			std::cerr << std::endl << "\t$ program behaviour file_name" << std::endl;
+			std::cerr << std::endl << "\tbehaviour = create_empty, check, solved, solve." << std::endl << std::endl;
+			exit(EXIT_FAILURE);
 	exit(EXIT_SUCCESS);
 }
