ae74993a1b799e36158c37d21cdb260676ca7f9b
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-01-07 12:20:41 -0200

    Correction, it was added the wrong Catch2 file... UtilsLibTest changed to test Catch2. All tests are     commented out.
    Tests will be updated to use Catch2 framework.

--- UtilsLibTest/source/utilsLibTest.cpp
+++ UtilsLibTest/source/utilsLibTest.cpp
@@ -1,57 +1,70 @@
-#define BOOST_TEST_MODULE TestModule
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
 
-#include <vector>
-
-#include <Test.h>
-#include <PossibleValues.h>
-
-TestCase( CheckEqualAndCheckClose )
+TEST_CASE( "catch test" )
 {
-	double i = 0;
-	double j = 0;
-
-	checkClose( i , j , 1.0e-12 );
-	checkEqual( i , j );
+	int a,b;
+	a = 3;
+	b = 3;
+	CHECK( a==b );
+	a = 4;
+	b = 5;
+	CHECK( a==b);
 }
+//#define BOOST_TEST_MODULE TestModule
 
-TestCase( cpp_11_foreach ){
-	std::vector<double> values;
-	int nValues = 5;
-	double rawValues[] = { 2.71, 3.14, 0.0, -3.14, -2.71 };
+//#include <vector>
 
-	for( int i = 0; i < nValues; ++i ){
-		values.push_back( rawValues[ i ] );
-	}
+//#include <Test.h>
+//#include <PossibleValues.h>
 
-	int counter = 0;
-	for( double v: values ){
-		checkClose( rawValues[ counter ], v, 1.0e-12 );
-		++counter; 
-	}
-}
+//TestCase( CheckEqualAndCheckClose )
+//{
+	//double i = 0;
+	//double j = 0;
 
-TestCase( PossibleValuesConstructor )
-{
-	std::set<unsigned> testSet;
-	std::vector<unsigned> setValues = {1,2,3,4,5,6,7,8,9};
-	for(unsigned value : setValues)
-		testSet.insert(value);
-	check(static_cast< std::set<unsigned> >(PossibleValues()) == testSet);
-}
+	//checkClose( i , j , 1.0e-12 );
+	//checkEqual( i , j );
+//}
 
-TestCase( UniquePossibleValue )
-{
-	PossibleValues pv;
-	std::vector<unsigned> valuesToBeErased = {1,2,3,4,6,7,8,9};
-	unsigned uniqueValue = 5;
-	for( unsigned value : valuesToBeErased )
-		pv.erase(value);
-	check( pv.uniqueValue() == uniqueValue );
-}
+//TestCase( cpp_11_foreach ){
+	//std::vector<double> values;
+	//int nValues = 5;
+	//double rawValues[] = { 2.71, 3.14, 0.0, -3.14, -2.71 };
 
-TestCase( PossibleValues_constants )
-{
-	checkEqual( PossibleValues::firstValue , 1 );
-	checkEqual( PossibleValues::lastValue , 9 );
-	checkEqual( PossibleValues::numberOfValues , 9 );
-}
+	//for( int i = 0; i < nValues; ++i ){
+		//values.push_back( rawValues[ i ] );
+	//}
+
+	//int counter = 0;
+	//for( double v: values ){
+		//checkClose( rawValues[ counter ], v, 1.0e-12 );
+		//++counter; 
+	//}
+//}
+
+//TestCase( PossibleValuesConstructor )
+//{
+	//std::set<unsigned> testSet;
+	//std::vector<unsigned> setValues = {1,2,3,4,5,6,7,8,9};
+	//for(unsigned value : setValues)
+		//testSet.insert(value);
+	//check(static_cast< std::set<unsigned> >(PossibleValues()) == testSet);
+//}
+
+//TestCase( UniquePossibleValue )
+//{
+	//PossibleValues pv;
+	//std::vector<unsigned> valuesToBeErased = {1,2,3,4,6,7,8,9};
+	//unsigned uniqueValue = 5;
+	//for( unsigned value : valuesToBeErased )
+		//pv.erase(value);
+	//check( pv.uniqueValue() == uniqueValue );
+//}
+
+//TestCase( PossibleValues_constants )
+//{
+	//checkEqual( PossibleValues::firstValue , 1 );
+	//checkEqual( PossibleValues::lastValue , 9 );
+	//checkEqual( PossibleValues::numberOfValues , 9 );
+//}
