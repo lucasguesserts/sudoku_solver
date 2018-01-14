eb272ba735433827cddcf98ed2ec3eb699db00c5
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-01-14 20:17:29 -0200

    A example in ElementLibTest changed.

--- ElementLibTest/source/ElementLibTest.cpp
+++ ElementLibTest/source/ElementLibTest.cpp
@@ -357,18 +357,16 @@ TestCase( "SudokuGame_isSolved", "[sudoku game]" )
 
 TestCase( "solve for one possible value in groups", "[sudoku game]" ) 
 {
-	const char * fileName = "one_possible_value_in_groups_example.sudokugame";
-	const char * datasetName = "Problem";
 	std::vector< std::vector<unsigned> > game = {
-		{0,8,7,0,0,0,0,0,0},
-		{0,0,2,0,0,7,0,0,8},
-		{0,0,0,0,5,8,0,7,3},
-		{0,3,1,9,0,4,0,0,0},
-		{0,0,4,0,7,0,2,0,0},
-		{0,0,0,5,0,6,8,1,0},
-		{9,1,0,7,4,0,0,0,0},
-		{2,0,0,3,0,0,7,0,0},
-		{0,0,0,0,0,0,4,5,0}
+		{0,9,1,0,0,0,0,5,0},
+		{4,0,0,3,0,0,8,0,9},
+		{2,0,0,0,0,0,4,1,0},
+		{0,7,0,9,0,0,0,0,0},
+		{0,0,0,0,7,0,0,0,0},
+		{0,0,0,0,0,8,0,4,0},
+		{0,2,5,0,0,0,0,0,1},
+		{8,0,7,0,0,1,0,0,3},
+		{0,6,0,0,0,0,2,9,0}
 	};
 	SudokuGame sg;
 	sg.set(game);
